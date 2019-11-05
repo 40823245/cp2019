166e1dfd0987ece262105efbc8a0396b37da80f4
40823245 <40823245@gm.nfu.edu.tw>
2019-11-05 12:23:25 +0800

    add pelican blog settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
+AUTHOR = '40823245'
 SITENAME = 'CP 2019 40823245'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
