83afec0829078eb6e26debb5d6cd15868208f7e8
40823245 <40823245@gm.nfu.edu.tw>
2019-11-26 22:40:12 +0800

    CF

--- downloads/dart_ex/CF/CF.dart
+++ downloads/dart_ex/CF/CF.dart
@@ -70,8 +70,8 @@ int calculatetemperatures(int temperatures) {
 /*
 html
 
-°C <input type="number" id="temperatures" value="0" min="0" max="100"> 
-temperatures
+temperatures<input type="number" id="temperatures" value="0" min="0" max="100"> 
+°C
 <input type="button" id="submit" value="Calculate"/>
 <br/>°F: <label id="output"></label>
 
