aeb7cb5569034eabd4e4146a4169d4eac6f5f42c
40823245 <40823245@gm.nfu.edu.tw>
2019-11-26 17:05:31 +0800

    CF

--- downloads/dart_ex/CF/CF.dart
+++ downloads/dart_ex/CF/CF.dart
@@ -1,3 +1,48 @@
+/*
+void main() {
+ print("Hello, World!");
+}
+*/
+
+/*
+ * 主函式也可以利用 => 定義單行的函式內容
+main() => print("Hello, World!");
+*/
+
+/*
+import 'dart:math' as math;
+
+void main() {
+  var n = 0; // °F
+  print("The °F increases as:\n");
+  for (int temperatures = 0; temperatures <= 10; temperatures++) {
+    n =(temperatures*1.8+32).round().toInt();
+    print("After $temperatures temperatures:\t $n temperatures");
+  }
+}
+*/
+
+/*
+import 'dart:math' as math;
+
+int temperatures = 0;
+const int NO_temperatures = 10;
+
+void main() {
+  print("The °F increases as:\n");
+  for (int temperatures = 0) {
+    rabbitCount = calculateRabbits(temperatures);
+    print("Count $temperatures temperatures:\t $temperaturesCount temperatures");
+  }
+}
+
+int calculatetemperatures(int temperatures) {
+  return(temperatures*1.8+32)
+      .round()
+      .toInt();
+}
+*/
+
 import "dart:html";
 
 
@@ -21,3 +66,61 @@ int calculatetemperatures(int temperatures) {
       .round()
       .toInt();
 }
+
+/*
+html
+
+°C <input type="number" id="temperatures" value="0" min="0" max="100"> 
+temperatures
+<input type="button" id="submit" value="Calculate"/>
+<br/>°F: <label id="output"></label>
+
+css
+
+body {
+  color: white;
+  font-size: 50px;
+}
+
+input, select, textarea {
+font-size: 100%;
+}
+*/
+/*
+import 'dart:html';
+
+InputElement task;
+UListElement list;
+
+main() {
+  task = querySelector('#task');
+  list = querySelector('#list');
+  task.onChange.listen((e) => addItem());
+}
+
+void addItem() {
+  var newTask = LIElement();
+  newTask.text = task.value;
+  task.value = '';
+  list.children.add(newTask);
+}
+*/
+
+/*
+html
+  
+  <input id="task" type="text" placeholder=
+  "What do you want to do?"/>
+  <ul id="list"/>
+  
+css
+
+body {
+  color: white;
+  font-size: 50px;
+}
+
+input, select, textarea {
+font-size: 100%;
+}
+*/
