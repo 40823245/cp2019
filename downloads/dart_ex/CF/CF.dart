b83bc2269d4bc34d03a7aebf533e007a8d43a034
40823245 <40823245@gm.nfu.edu.tw>
2019-11-26 17:18:38 +0800

    CF

--- downloads/dart_ex/CF/CF.dart
+++ downloads/dart_ex/CF/CF.dart
@@ -1,3 +1,48 @@
+/*
+void main() {
+ print("Hello, World!");
+}
+*/
+
+/*
+ * 主函式也可以利用 => 定義單行的函式內容
+main() => print("Hello, World!");
+*/
+
+/*
+import 'dart:math' as math;
+
+void main() {
+  var n = 0; // °F
+  print("The °F increases as:\n");
+  for (int temperatures = 0; temperatures <= 10; temperatures++) {
+    n =(temperatures*1.8+32).round().toInt();
+    print("After $temperatures temperatures:\t $n temperatures");
+  }
+}
+*/
+
+/*
+import 'dart:math' as math;
+
+int temperatures = 0;
+const int NO_temperatures = 10;
+
+void main() {
+  print("The °F increases as:\n");
+  for (int temperatures = 0) {
+    rabbitCount = calculateRabbits(temperatures);
+    print("Count $temperatures temperatures:\t $temperaturesCount temperatures");
+  }
+}
+
+int calculatetemperatures(int temperatures) {
+  return(temperatures*1.8+32)
+      .round()
+      .toInt();
+}
+*/
+
 import "dart:html";
 
 
